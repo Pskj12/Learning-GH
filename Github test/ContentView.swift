271c3a521a95f8b6963977a327d404d48c11ba7e
Pskj12 <122931976+Pskj12@users.noreply.github.com>
2023-01-26 21:32:14 +0100

    Test aaaaaa
    Test aaaaaa

--- Github test/ContentView.swift
+++ Github test/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, Cringecrew!")
+            Text("Sup, Cringecrew!")
         }
         .padding()
     }
