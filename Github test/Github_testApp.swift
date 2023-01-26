6db9162761b680d5888931d6d8abd4ed5ca190dd
Petter Joys <Petterjoys@gmail.com>
2023-01-26 21:04:08 +0100

    Initial Commit

---
+++ Github test/Github_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_testApp.swift
+//  Github test
+//
+//  Created by Petter Joys on 26/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Github_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
